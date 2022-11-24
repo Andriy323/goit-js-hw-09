2db00c3b99800e4e7e38a59fffdeaba27f4f96ce
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2022-11-24 20:36:26 +0200

    finish dz 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+
+refs.start.addEventListener('click', startChangeColor);
+refs.stop.addEventListener('click', stopChangeColor);
+refs.stop.setAttribute('disabled', 'disabled');
+let timerId = null;
+
+function startChangeColor() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  console.log(timerId);
+  refs.start.setAttribute('disabled', 'disabled');
+  refs.stop.removeAttribute('disabled');
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  refs.start.removeAttribute('disabled');
+  refs.stop.setAttribute('disabled', 'disabled');
+
+  console.log('STOP');
+}
